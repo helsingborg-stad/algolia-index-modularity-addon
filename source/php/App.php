1c02b44205eb825a620dbfb2c016222c54777b86
Sebastian Thulin <sebastian.thulin@helsingborg.se>
2020-04-20 09:13:53 +0200

    Removes mid-* posttypes from indexable posttypes.

--- source/php/App.php
+++ source/php/App.php
@@ -9,5 +9,32 @@ class App
         //Add rendered modules to content
 
         //Remove mod-* posttypes from indexable results. 
+
+        //add_filter('AlgoliaIndex/Record', array($this, 'addModularityModule')); 
+        add_filter('AlgoliaIndex/IndexablePostTypes', array($this, 'removeModularityPostTypes')); 
+    }
+
+    public function addModularityModule () {
+
+    }
+
+    /**
+     * Remove mod-* posttypes
+     *
+     * @param array $postTypes
+     * @return array $postTypes
+     */
+    public function removeModularityPostTypes ($postTypes) {
+        //Remove mod-* posttypes 
+        if(is_array($postTypes) && !empty($postTypes)) {
+            $filteredResult = array();
+            foreach($postTypes as $postType) {
+                if(substr($postType, 0, 4) != "mod-") {
+                    $filteredResult[] = $postType;  
+                }
+            }
+            return $filteredResult; //Return filtered
+        }
+        return $postTypes; //Not valid, return original
     }
 }
